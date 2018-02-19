93c55c2a47b2cf72fd23eb0e1d8a196af7b40cec
CHAMPION ChPRODUCTIONS <jiltedflower@outlook.com>
2018-02-19 14:24:50 -0800

    commit

--- app/src/debug/java/comjiltedflowergithub/safezone/SkillsActivity.kt
+++ app/src/debug/java/comjiltedflowergithub/safezone/SkillsActivity.kt
@@ -1,34 +1,38 @@
 package comjiltedflowergithub.safezone
 
-import android.os.Bundle
+//import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_skills.*
+import comjiltedflowergithub.safezone.R.layout.activity_skills
+//import kotlinx.android.synthetic.main.activity_skills.*
 
-class SkillsActivity : AppCompatActivity() {
+//class SkillsActivity : AppCompatActivity() {
+//
+//    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+//        when (item.itemId) {
+//            R.id.navigation_home -> {
+//                message.setText(R.string.title_home)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//            R.id.navigation_dashboard -> {
+//                message.setText(R.string.title_dashboard)
+//                return@OnNavigationItemSelectedListener true
+ //           }
+//            R.id.navigation_notifications -> {
+//                message.setText(R.string.title_notifications)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//        }
+//        false
+//    }
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(activity_skills)
+//
+ //       navigation.github.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+//
+//    }
+//
+//}
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_skills)
-
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-    }
-}
--- app/src/main/java/comjiltedflowergithub/safezone/GMapsActivity.kt
+++ app/src/main/java/comjiltedflowergithub/safezone/GMapsActivity.kt
@@ -11,6 +11,10 @@ import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
+        // App Center SDK
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class GMapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -23,6 +27,11 @@ class GMapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        // App Center start SDK
+        AppCenter.start(application, "ec0ecbbf-3970-4f8c-b0aa-62e52b6da599",
+                Analytics::class.java, Crashes::class.java)
+        AppCenter.start(application, "ec0ecbbf-3970-4f8c-b0aa-62e52b6da599",
+                Analytics::class.java, Crashes::class.java)
     }
 
     /**
